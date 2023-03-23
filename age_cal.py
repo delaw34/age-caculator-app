f526bd31574f7dd0a74cb1e5fbaf36e649d9bbf3
delaw34 <delaw34@yahoo.com>
2023-03-23 14:56:50 +0100

    updated version to deny access to any person younger than 18 years

--- age_cal.py
+++ age_cal.py
@@ -1,3 +1,7 @@
+# Application that can check the age of a person
+# Input name and age in the application
+# 18 years below access will be denied
+
 current_year = 2023 #int
 
 
@@ -5,10 +9,13 @@ current_year = 2023 #int
 def age_cal():
     name_of_student = (input("Enter Name:   "))
     age_calculation = int(input("Enter year of birth:   "))
-    if age_calculation :
-    
-        final_year = current_year - age_calculation
+    if  age_calculation < 2015 :
+        final_year  = current_year - age_calculation
+        
         print("hello", name_of_student , "your age is", final_year ,"years")
+    else:
+        final_year  = current_year - age_calculation
+        print('hello', name_of_student, 'your age is', final_year, "years, you are not qualified yet" )
   
 
 age_cal()
