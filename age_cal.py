5e7c5d1399af93f0c4572664d2244cbd39260de5
delaw34 <delaw34@yahoo.com>
2023-03-23 09:10:01 +0100

    Initial commit of age calculator

---
+++ age_cal.py
@@ -0,0 +1,14 @@
+current_year = 2023 #int
+
+
+
+def age_cal():
+    name_of_student = (input("Enter Name:   "))
+    age_calculation = int(input("Enter year of birth:   "))
+    if age_calculation :
+    
+        final_year = current_year - age_calculation
+        print("hello", name_of_student , "your age is", final_year ,"years")
+  
+
+age_cal()
