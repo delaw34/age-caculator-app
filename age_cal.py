1e17b31de60b86bd8878e355708c1c313e3c211e
Lawrence Akpan <117764127+delaw34@users.noreply.github.com>
2023-04-04 01:48:14 +0100

    Update age_cal.py

--- age_cal.py
+++ age_cal.py
@@ -9,7 +9,7 @@ current_year = 2023 #int
 def age_cal():
     name_of_student = (input("Enter Name:   "))
     age_calculation = int(input("Enter year of birth:   "))
-    if  age_calculation <= 2015 :
+    if  age_calculation <= 2005 :
         final_year  = current_year - age_calculation
         
         print("hello", name_of_student , "your age is", final_year ,"years and qualified")
